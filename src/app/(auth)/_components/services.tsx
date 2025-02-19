478da199fb6dc4366142e2bc65237d3a545f7b66
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-02-19 11:57:59 -0300

    adding services section

--- src/app/(auth)/_components/services.tsx
+++ src/app/(auth)/_components/services.tsx
@@ -1,4 +1,31 @@
 export const Services = () => {
+   const servicesData = [
+      {
+         id: 1,
+         name: 'Clareamento Dental',
+         description: 'Indicado para quem deseja um sorriso mais brilhante e saudável duradouro.',
+         img: 'services-1.jpg'
+      },
+      {
+         id: 2,
+         name: 'Implante Dental',
+         description: 'Utilizamos materiais de alta qualidade para garantir durabilidade e um sorriso natural.',
+         img: 'services-2.jpg'
+      },
+      {
+         id: 3,
+         name: 'Ortodontia',
+         description: 'Oferecemos tratamentos com aparelhos fixos, móveis e alinhadores transparentes.',
+         img: 'services-3.jpg'
+      },
+      {
+         id: 4,
+         name: 'Limpeza Profissional',
+         description: 'Remove tártaro e placas bacterianas, prevenindo cáries e doenças gengivais',
+         img: 'services-4.jpg'
+      }
+   ];
+
 
    return (
       <section id="services">
@@ -7,13 +34,17 @@ export const Services = () => {
             <h1 className="section-title">Serviços de <span>alta qualidade</span> para você</h1>
             <p className="section-desc">Estamos comprometidos com a sustentabilidade. iniciativas ecológicas.</p>
             <div className="services-list">
-               <div className="service">
-                  <img src="./services/services-1.jpg" alt="pessoa em uma consulta odontológica" className="service-img" />
-                  <img src="" alt="" />
-                  <p className="service-title">Implante dental</p>
-                  <p className="service-desc">lorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
-                  <p>Ver mais</p>
-               </div>
+               {servicesData.map((item) => (
+                  <div className="service" key={item.id}>
+                     <img src={`./services/${item.img}`} alt="pessoa em uma consulta odontológica" className="service-img" />
+                     <img src="" alt="" />
+                     <div className="service-infos">
+                        <p className="service-title">{item.name}</p>
+                        <p className="service-desc">{item.description}</p>
+                        <p className="read-more">Ver mais <img src="./svgs/arrow-sm-right-svgrepo-com.svg" alt="arrow right" /></p>
+                     </div>
+                  </div>
+               ))}
             </div>
          </div>
       </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,20 @@ import { Hero } from "./(auth)/_components/hero";
 import { Emphasis } from "./(auth)/_components/emphasis";
 import { About } from "./(auth)/_components/about";
 import { Services } from "./(auth)/_components/services";
+import { Faixa } from "./(auth)/_components/faixa";
 
 export default function Home() {
    return (
-      <main>
+      <main className="main">
          <Header/>
          <Hero/>
          <Emphasis/>
          <About/>
          <Services/>
+         <Faixa/>
+         <div className="up-btn">
+            <img src="./svgs/arrow-down-svgrepo-com.svg" alt="" />
+         </div>
       </main>
    );
 }
